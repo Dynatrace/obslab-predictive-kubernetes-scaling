10f341894c0fb91dfacb14b0a70269ae8097a14e
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-08-29 09:04:01 +1000

    Add creation ping

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -37,3 +37,13 @@ kubectl cluster-info --context kind-predictive-kubernetes-scaling-demo
 
 dynatrace/init.sh
 apps/init.sh
+
+# Creation Ping
+curl -X POST https://grzxx1q7wd.execute-api.us-east-1.amazonaws.com/default/codespace-tracker \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"tenant\": \"$DYNATRACE_ENVIRONMENT_ID\",
+    \"repo\": \"$GITHUB_REPOSITORY\",
+    \"demo\": \"obslab-predictive-kubernetes-scaling\",
+    \"codespace.name\": \"$CODESPACE_NAME\"
+  }"
