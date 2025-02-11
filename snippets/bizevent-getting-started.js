286eba05f1e320463cef2ff4f00d593913ba6395
Adam Gardner <adam.gardner@dynatrace.com>
2025-02-12 09:29:36 +1000

    Deployed 83693db with MkDocs version: 1.6.0

--- snippets/bizevent-getting-started.js
+++ snippets/bizevent-getting-started.js
@@ -1,5 +1,5 @@
 <script>
 document.addEventListener('DOMContentLoaded', function() {
-  dynatrace.sendBizEvent('page_load', {"page": "getting-started"});
+  dynatrace.sendBizEvent('page_load', {"page": "2. getting-started"});
 });
 </script>
--- snippets/bizevent-resources.js
+++ snippets/bizevent-resources.js
@@ -1,5 +1,5 @@
 <script>
 document.addEventListener('DOMContentLoaded', function() {
-  dynatrace.sendBizEvent('page_load', {"page": "resources"});
+  dynatrace.sendBizEvent('page_load', {"page": "4. resources"});
 });
 </script>
