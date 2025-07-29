1ea39a1bb9d23161f5e2646f09e85f92019d47ad
Adam Gardner <adam.gardner@dynatrace.com>
2025-07-29 12:38:00 +1000

    Deployed b8cfe24 with MkDocs version: 1.6.1

--- snippets/bizevent-whats-next.js
+++ snippets/bizevent-whats-next.js
@@ -1,5 +1,7 @@
 <script>
+var CURRENT_PAGE = "5. whats-next";
+
 document.addEventListener('DOMContentLoaded', function() {
-  dynatrace.sendBizEvent('page_load', {"page": "5. whats-next"});
+  dynatrace.sendBizEvent('page_load', {"page": CURRENT_PAGE});
 });
 </script>
